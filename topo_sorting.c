f319aa0049c046cf74c3b1957106325eb951f30e
VictorGee <victorg@ufrj.br>
2015-04-20 14:01:51 -0300

    Algoritmo de Ordenação Topológica já implementado
    Falta dar free nas listas de adjacências...

--- topo_sorting.c
+++ topo_sorting.c
@@ -2,53 +2,84 @@
 #include <stdlib.h>
 
 typedef struct _NO {
-	int chave;
-	struct _NO* prox;
+    int chave;
+    struct _NO* prox;
 } no;
 
-int TAM;
-no* lista;
+int TAM, aresta, vertice, key;
+no* vizinhosDeEntrada;
+no* vizinhosDeSaida;
 
-void init();
-void insere(int vertice, int chave);
+no* init(no* lista);
+void insere(no* lista, int vertice, int chave);
+//void imprime(no* lista);
 
 int main(void) {
-	init();
-    if (lista == NULL) {
-        printf("Erro na criacao da lista!!!\n");
+    scanf("%d %d", &TAM, &aresta);
+    int saida[TAM];
+    int i, k = 0;
+    vizinhosDeEntrada = init(vizinhosDeEntrada);
+    vizinhosDeSaida = init(vizinhosDeSaida);
+    if (vizinhosDeEntrada == NULL) {
+        printf("Erro na criacao da lista da entrada!!!\n");
         exit(EXIT_FAILURE);
     }
-
-	for(i = 1; i < TAM + 1; i ++) {
-        int key;
-        scanf("%d", &n);
-        for(j = 0; j < n; j ++) {
-            scanf("%d", &key);
-            insere(i,key);
-        }
+    else if (vizinhosDeSaida == NULL) {
+        printf("Erro na criacao da lista de saida!!!\n");
+        exit(EXIT_FAILURE);
+    }
+    for(i = 1; i < aresta + 1; i ++) {
+        scanf("%d %d",&vertice, &key);
+        insere(vizinhosDeEntrada,vertice,key);
+        insere(vizinhosDeSaida,key,vertice);
     }
 
-    free(lista);
+//  imprime(vizinhosDeEntrada);
+//  printf("\n");
+//  imprime(vizinhosDeSaida);
 
-	return 0;
+    int objeto = TAM;
+    while(objeto != 0) {
+        for(i = 1; i < TAM + 1; i ++) {
+            if(vizinhosDeEntrada[i].chave == 0) {
+                no* aux = vizinhosDeSaida[i].prox;
+                    while(aux != NULL) {
+                        vizinhosDeEntrada[aux->chave].chave--;
+                        aux = aux->prox;
+                    }
+                saida[k] = i;
+                vizinhosDeEntrada[i].chave = -1;
+                k++;
+                objeto--;
+            }
+        }
+   }
+
+
+    for(i = 0; i < k; i ++) {
+        printf("%d ", saida[i]);
+    }
+
+    return 0;
 }
 
-void init() {
-	int i;
+no* init(no* lista) {
+    int i;
 
-	scanf("%d", &TAM);
-	lista = (no*) malloc(sizeof(no)*(TAM+1));
-	for (i = 1; i < TAM + 1; i ++){
+    lista = (no*) malloc(sizeof(no)*(TAM+1));
+    for (i = 1; i < TAM + 1; i ++){
         lista[i].chave = 0;
         lista[i].prox = NULL;
-	}
+    }
+
+    return lista;
 }
 
-void insere(int vertice, int chave) {
-	no* aux;
-	aux = (no*) malloc(sizeof(no));
-	aux->prox= NULL;
-	aux->chave = chave;
+void insere(no* lista,int vertice, int chave) {
+    no* aux;
+    aux = (no*) malloc(sizeof(no));
+    aux->prox= NULL;
+    aux->chave = chave;
 
     if(lista[vertice].prox == NULL) {
         lista[vertice].prox = aux;
@@ -62,5 +93,21 @@ void insere(int vertice, int chave) {
         }
         temp->prox = aux;
     }
-	lista[vertice].chave++;
+    lista[vertice].chave++;
 }
+
+//void imprime(no* lista) {
+//    int i;
+//    no* tmp;
+//
+//    for(i = 1; i< TAM + 1; i ++) {
+//        tmp = lista[i].prox;
+//        printf("%2d: (%d) ==> ", i, lista[i].chave);
+//        while (tmp != NULL) {
+//        printf("%d  ", tmp->chave);
+//        tmp = tmp->prox;
+//    }
+//
+//        printf("\n");
+//    }
+//}
