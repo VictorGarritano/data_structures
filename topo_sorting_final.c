929e6798684c52da3f0769c2eab3239edd14ae13
VictorGee <victorg@ufrj.br>
2015-04-22 15:30:05 -0300

    Commit final(?)
    Estou dando push na versão final do programa (topo_sorting_final.c) e
    colocando as duas versões dos algoritmos de ordenação topológica no
    topo_sorting.c.
    A primeira é a mais rápida, rodando em O(n + m) e a segunda rodando em
    O(n^2 + m)

--- topo_sorting_final.c
+++ topo_sorting_final.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 typedef struct _NO {
-    int chave;
-    struct _NO* prox;
+	int chave;
+	struct _NO* prox;
 } no;
 
 int TAM, aresta, vertice, key;
@@ -13,14 +13,12 @@ no* vizinhosDeSaida;
 no* init(no* lista);
 void insere(no* lista, int vertice, int chave);
 void libera(no* lista);
-// void imprime(no* lista);
 
 int main(void) {
     scanf("%d %d", &TAM, &aresta);
-    int saida[TAM];
-    int i, k = 0;
-    vizinhosDeEntrada = init(vizinhosDeEntrada);
-    vizinhosDeSaida = init(vizinhosDeSaida);
+    int i;
+	vizinhosDeEntrada = init(vizinhosDeEntrada);
+	vizinhosDeSaida = init(vizinhosDeSaida);
     if (vizinhosDeEntrada == NULL) {
         printf("Erro na criacao da lista da entrada!!!\n");
         exit(EXIT_FAILURE);
@@ -30,16 +28,11 @@ int main(void) {
         exit(EXIT_FAILURE);
     }
     for(i = 1; i < aresta + 1; i ++) {
-        scanf("%d %d",&vertice, &key);
+       	scanf("%d %d",&vertice, &key);
         insere(vizinhosDeEntrada,vertice,key);
         insere(vizinhosDeSaida,key,vertice);
     }
-/*
-Roda em O(n + m):
-->Esse não precisa percorrer a lista inteira para encontrar uma nova fonte...
-->Esse for que segue é mais um pré-processamento, que será nossa lista encadeada de fontes.
-    Iteraremos por ela ao invés de percorrermos a lista toda quando precisarmos de uma nova fonte.
-
+    
     for(i = 1; i < TAM + 1; i ++) {
         if(vizinhosDeEntrada[i].chave == 0) {
             insere(vizinhosDeEntrada, 0, i);
@@ -60,69 +53,39 @@ Roda em O(n + m):
         aux1 = aux1->prox;
     }
     free(aux1);
-
-->Imprime a lista de fontes
-
-    no* temp = vizinhosDeEntrada[0].prox;
-    while(temp != NULL) {
-        printf("%d ", temp->chave);
-        temp = temp->prox;
-    }
-    free(temp);
-
-*/
-
-
-/* Roda em tempo O(n^2 + m): 
-n * T("encontrar a fonte") + m(nº de arestas)...
     
-    int objeto = TAM;
-    while(objeto != 0) {
-        for(i = 1; i < TAM + 1; i ++) {
-            if(vizinhosDeEntrada[i].chave == 0) {
-                no* aux = vizinhosDeSaida[i].prox;
-                saida[k] = i;
-                vizinhosDeEntrada[i].chave = -1;
-                k++;
-                objeto--;
-                while(aux != NULL) {
-                    vizinhosDeEntrada[aux->chave].chave--;
-                    aux = aux->prox;
-                }
-            }
-        }
-    }
-
-*/
-    for(i = 0; i < k; i ++) {
-        printf("%d ", saida[i]);
-    }
+    no* temp = vizinhosDeEntrada[0].prox;
+	while(temp != NULL) {
+		printf("%d ", temp->chave);
+		temp = temp->prox;
+	}
+	free(temp);
 
     libera(vizinhosDeEntrada);
     libera(vizinhosDeSaida);
     vizinhosDeEntrada = NULL;
     vizinhosDeSaida = NULL;
 
-    return 0;
+	return 0;
 }
 
 no* init(no* lista) {
-    int i;
+	int i;
 
-    lista = (no*) malloc(sizeof(no)*(TAM+1));
-    for (i = 1; i < TAM + 1; i ++){
+	lista = (no*) malloc(sizeof(no)*(TAM+1));
+	for (i = 1; i < TAM + 1; i ++){
         lista[i].chave = 0;
         lista[i].prox = NULL;
-    }
+	}
 
-    return lista;
+	return lista;
 }
 
 void insere(no* lista,int vertice, int chave) {
-    no* aux;
-    aux = (no*) malloc(sizeof(no));
-    aux->prox= NULL;
-    aux->chave = chave;
+	no* aux;
+	aux = (no*) malloc(sizeof(no));
+	aux->prox= NULL;
+	aux->chave = chave;
 
     if(lista[vertice].prox == NULL) {
         lista[vertice].prox = aux;
@@ -136,7 +99,7 @@ void insere(no* lista,int vertice, int chave) {
         }
         temp->prox = aux;
     }
-    lista[vertice].chave++;
+	lista[vertice].chave++;
 }
 
 void libera(no* lista) {
@@ -151,17 +114,3 @@ void libera(no* lista) {
     }
     free(lista);
 }
-
-// void imprime(no* lista){
-//     int i;
-//     no* tmp;
-//     for(i = 1; i < TAM + 1; i++) {
-//         tmp = lista[i].prox;
-//         printf("%2d: (%d) ==> ", i, lista[i].chave);
-//         while (tmp != NULL) {
-//             printf("%d  ", tmp->chave);
-//             tmp = tmp->prox;
-//         }
-//         printf("\n");
-//     }
-// }
